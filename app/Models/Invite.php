657e37d133cc10dd1f50495df28e2cdd9baeead8
Phu Hoa <phuhoadn95@gmail.com>
2017-01-07 16:31:56 +0700

    create model and migration

---
+++ app/Models/Invite.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Invite extends Model
+{
+    protected $fillable = [
+        'sender_id',
+        'recevier_id',
+        'survey_id',
+    ];
+
+    public function sender()
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    public function recevier()
+    {
+        return $this->belongsTo(User::class, 'recevier_id');
+    }
+
+    public function survey()
+    {
+        return $this->belongsTo(Survey::class);
+    }
+}
---
+++ app/Models/Survey.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Survey extends Model
+{
+    protected $fillable = [
+        'title',
+        'user_id',
+    ];
+
+    public function invites()
+    {
+        return $this->hasMany(Invite::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function questions()
+    {
+        return $this->hasMany(Question::class);
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
+
+    public function setLikeAttributes($value)
+    {
+        $this->attributes['like'] = $value ?: config('settings.like_default');
+    }
+}
