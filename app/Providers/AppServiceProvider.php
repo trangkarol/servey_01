37c1093fdffa7d7337eb19e47059a9fbc676758e
Phu Hoa <phuhoadn95@gmail.com>
2017-01-17 23:44:54 +0700

    file temp

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,18 @@ use Illuminate\Support\ServiceProvider;
 use App\Repositories\User\UserInterface;
 use App\Repositories\User\UserRepository;
 use App;
+use App\Repositories\Survey\SurveyInterface;
+use App\Repositories\Question\QuestionInterface;
+use App\Repositories\Answer\AnswerInterface;
+use App\Repositories\Survey\SurveyRepository;
+use App\Repositories\Question\QuestionRepository;
+use App\Repositories\Answer\AnswerRepository;
+use App\Repositories\Result\ResultInterface;
+use App\Repositories\Result\ResultRepository;
+use App\Repositories\Invite\InviteInterface;
+use App\Repositories\Invite\InviteRepository;
+use App\Repositories\Like\LikeInterface;
+use App\Repositories\Like\LikeRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,5 +39,11 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         App::bind(UserInterface::class, UserRepository::class);
+        App::bind(SurveyInterface::class, SurveyRepository::class);
+        App::bind(AnswerInterface::class, AnswerRepository::class);
+        App::bind(QuestionInterface::class, QuestionRepository::class);
+        App::bind(ResultInterface::class, ResultRepository::class);
+        App::bind(InviteInterface::class, InviteRepository::class);
+        App::bind(LikeInterface::class, LikeRepository::class);
     }
 }
---
+++ resources/lang/en/home.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    'delete' => 'Delete',
+    'logout' => 'Logout',
+    'list_survey' => 'LIST SURVEY',
+    'get_survey' => 'Get Survey',
+    'name_survey' => 'Survey name',
+    'add_option' => 'Add option',
+    'add_other' => 'Add other',
+    'checkboxes' => 'Checkboxes',
+    'short_answer' => 'Short answer',
+    'passage' => 'Passage',
+    'choices' => 'Choices',
+    'enter_answer_here' => 'Enter answer here',
+    'other' => ' Other:',
+    'home' => 'Home',
+    'notices' => 'Notices',
+    'setting' => 'Setting',
+    'search' => 'Search',
+    'setting' => 'setting',
+    'enter_question_here' => 'Enter question here',
+    'create_survey' => 'Create Survey',
+    'survey' => 'Survey',
+    'remove' => 'Remove',
+    'owned_by' => 'Owned by',
+    'survey' => 'Survey',
+    'date' => 'Date',
+    'no' => 'No',
+    'submit' => 'Submit',
+    'error' => 'Error',
+
+];
