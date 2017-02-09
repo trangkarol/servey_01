bb24ae14e1e0f0df3db6469de9fcea808618c9f6
MinhTri1911 <huynh.ngoc.minh.tri@framgia.com>
2017-02-09 08:51:49 +0700

    fix survey view

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ require('laravel-elixir-vue-2');
 elixir(mix => {
 
     mix.sass('app.scss')
-   .sass('site.scss', 'public/user/css/site.css')
+   .sass('main.scss', 'public/user/css/main.css')
    .sass('home.scss', 'public/user/css/home.css')
    .sass('admin-style-1.scss', 'public/admin/css/admin-style-1.css')
    .webpack('app.js')
@@ -26,14 +26,20 @@ elixir(mix => {
    .scripts('admin-script.js', 'public/admin/js/admin-script.js')
    .copy('resources/assets/fonts', 'public/admin/fonts')
    .copy([
+            'public/bower/bootstrap/dist/css/bootstrap.css',
+            'public/bower/bootstrap/dist/css/bootstrap.min.css'
+         ], 'public/admin/css')
+   .copy([
             'public/bower/bootstrap/dist/js/bootstrap.min.js',
             'public/bower/bootstrap/dist/js/bootstrap.js',
             'public/bower/jquery/dist/jquery.js'
          ], 'public/admin/js')
    .copy([
-            'public/bower/bootstrap/dist/css/bootstrap.css',
-            'public/bower/bootstrap/dist/css/bootstrap.min.css'
-         ], 'public/admin/css')
+            'public/bower/jquery/dist/jquery.min.js',
+            'public/bower/skel/dist/skel.min.js',
+            'resources/assets/js/util.js',
+            'resources/assets/js/main.js'
+         ], 'public/user/js')
    .browserSync({
         proxy: 'http://localhost:8000/',
         proxy: 'http://survey.com/'
