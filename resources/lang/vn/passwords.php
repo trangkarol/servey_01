c699a79adb437cd9813eaf695d5abc8eed24aedc
MinhTri1911 <huynh.ngoc.minh.tri@framgia.com>
2017-02-28 16:01:05 +0700

    trans VietNamese

---
+++ resources/lang/vn/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Mật khẩu phải có ít nhất 6 kí tự và phải trùng với trùng xác nhận mật khẩu.',
+    'reset' => 'Mật khẩu của bạn đã được cài đặt lại!',
+    'sent' => 'Chúng tôi đã gửi qua email link cài đặt lại mật khẩu của bạn!',
+    'token' => 'Thiết lập lại mật khẩu với token này là không hợp lệ.',
+    'user' => 'Chúng tôi không thể tìm thấy một người sử dụng với địa chỉ email này.',
+
+];
