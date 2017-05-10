7e3e6daa78e6d3889e4f35d93e89d88d9df7c3c5
phu hoa <phuhoadn95@gmail.com>
2017-05-10 18:16:23 +0700

    fixbug

--- resources/views/temps/text_other.blade.php
+++ resources/views/temps/text_other.blade.php
@@ -1,7 +1,10 @@
 @if (!$tempAnswers)
     {!! Form::textarea("answer[$idQuestion][$idAnswer]", '', [
-        'class' => 'box-orther js-elasticArea animated zoomIn form-control input' . $idQuestion,
+        'class' => 'box-orther required js-elasticArea animated zoomIn form-control input' . $idQuestion,
         'required' => true,
         'placeholder' => trans('home.answer_here'),
+    ]) !!}
+    {!! Form::label("answer[$idQuestion][$idAnswer]", trans('validation.msg.required'), [
+        'class' => 'error div-hidden',
     ]) !!}
 @endif
