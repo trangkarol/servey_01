9cfe545d68a160e2afaec8d95da8dcaeee66c110
Vinhbb96er <vinhbb96er@gmail.com>
2018-03-23 17:32:16 +0700

    delete-old-view

--- resources/views/user/blocks/validate.blade.php
+++
@@ -1,7 +0,0 @@
-@if (count($errors))
-    <div class="alert alert-danger warning-login-register">
-        @foreach ($errors->all() as $error)
-            <p>{{ $error }}</p>
-        @endforeach
-    </div>
-@endif
